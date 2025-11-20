d2713f42f6fd88131e76575033fafc5d2ebcc8ff
Sue Le Marinel <sue.lemarinel@gmail.com>
2025-11-20 12:29:40 +0100

    Routes signin & signup

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
+require('./models/connection');
 
-
-
-
-
-
-
-
+const express = require('express');
 const cors = require('cors');
+
+const app = express()
+
 app.use(cors());
+app.use(express.json());
+
+const userRouter = require('./routes/users');
+app.use('/users', userRouter);
+
+module.exports = app;
